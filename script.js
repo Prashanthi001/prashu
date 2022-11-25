4416062565a122b9fa647768c82f77f2b191dd66
prashanti <prashanthig148@gmail.com>
2022-11-25 15:55:04 +0530

    prashu

---
+++ script.js
@@ -0,0 +1,9 @@
+const show =()=>{
+     console.log('world')
+     document.getElementById('main1').style.display="flex"
+    
+}
+const cls=()=>{
+    console.log("hello")
+    document.getElementById('main1').style.display="none"
+}
